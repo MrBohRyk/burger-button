2b631a5e11b9e1617e585585c0ee0124067ad8a5
MrBohRyk <lyutyk2004@gmail.com>
2025-10-05 19:55:11 +0200

    song

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-document.getElementById('burger').addEventListener('click', () => {
-  document.getElementById('burger').classList.toggle('active');
+const burger = document.getElementById('burger');
+const music = document.getElementById('music');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+
+  if (burger.classList.contains('active')) {
+    music.play();   // ▶️ почати музику
+  } else {
+    music.pause();  // ⏸️ зупинити
+  }
 });
